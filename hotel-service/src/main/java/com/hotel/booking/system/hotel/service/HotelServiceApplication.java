35a60d70e0d7b273cc51a1c3a1bea915d8977afc
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-06-22 23:41:05 -0400

    chore: add `hotel-service` application

---
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/HotelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.system.hotel.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(HotelServiceApplication.class, args);
+  }
+
+}
