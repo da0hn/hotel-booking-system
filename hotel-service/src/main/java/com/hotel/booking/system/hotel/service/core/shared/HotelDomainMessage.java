9148988b2a59eb0cbe69129869bc1b0676f928c7
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-06-26 21:54:06 -0400

    squash! feat: add `entities` and `value objects` related to `Hotel`

---
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/core/shared/HotelDomainMessage.java
@@ -0,0 +1,16 @@
+package com.hotel.booking.system.hotel.service.core.shared;
+
+public final class HotelDomainMessage {
+
+  public static final String HOTEL_CATEGORY_NOT_NULL = "hotel.category.must.not.be.null";
+  public static final String HOTEL_LOCALITY_NOT_NULL = "hotel.locality.must.not.be.null";
+  public static final String HOTEL_NAME_INVALID = "hotel.name.must.not.be.null-or-empty";
+  public static final String HOTEL_DESCRIPTION_INVALID = "hotel.description.must.not.be.null-or-empty";
+  public static final String HOTEL_STREET_INVALID = "hotel.street.must.not.be.null-or-empty";
+  public static final String HOTEL_CEP_INVALID = "hotel.cep.invalid";
+  public static final String HOTEL_CATEGORY_NOT_FOUND = "hotel.category.not.found";
+  public static final String HOTEL_LOCALITY_NOT_FOUND = "hotel.locality.not.found";
+
+  private HotelDomainMessage() {}
+
+}
