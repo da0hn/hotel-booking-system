a2a1bf8866c7c2b9c0d595eb26c4f28abc66b6a2
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-06-28 23:24:01 -0400

    feat: add interface `HotelDatabaseMapper` and implementation

--- hotel-service/src/main/java/com/hotel/booking/system/hotel/service/data/db/mapper/HotelDatabaseMapper.java
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/data/db/mapper/HotelDatabaseMapper.java
@@ -2,77 +2,22 @@ package com.hotel.booking.system.hotel.service.data.db.mapper;
 
 import com.hotel.booking.system.hotel.service.core.domain.entity.Hotel;
 import com.hotel.booking.system.hotel.service.core.domain.entity.Room;
-import com.hotel.booking.system.hotel.service.core.domain.entity.Rooms;
-import com.hotel.booking.system.hotel.service.core.domain.valueobject.*;
-import com.hotel.booking.system.hotel.service.data.db.entity.HotelCategoryEntity;
 import com.hotel.booking.system.hotel.service.data.db.entity.HotelEntity;
-import com.hotel.booking.system.hotel.service.data.db.entity.LocalityEntity;
 import com.hotel.booking.system.hotel.service.data.db.entity.RoomEntity;
-import org.springframework.stereotype.Component;
 
-@Component
-public class HotelDatabaseMapper {
+import java.util.Collection;
+import java.util.Set;
 
-  public Hotel hotelEntityToHotel(final HotelEntity hotelEntity) {
-    return Hotel.builder()
-      .id(HotelId.of(hotelEntity.getId()))
-      .name(hotelEntity.getName())
-      .description(hotelEntity.getDescription())
-      .categoryId(HotelCategoryId.of(hotelEntity.getCategory().getId()))
-      .localityId(LocalityId.of(hotelEntity.getId()))
-      .address(new HotelAddress(hotelEntity.getHotelCep(), hotelEntity.getHotelStreet()))
-      .rooms(Rooms.newInstance(
-        hotelEntity.getRooms().stream()
-          .map(this::roomEntityToRoom)
-          .toList()
-      ))
-      .build();
-  }
+public interface HotelDatabaseMapper {
 
-  public HotelEntity hotelToHotelEntity(final Hotel hotel) {
-    return HotelEntity.builder()
-      .id(hotel.getId().getValue())
-      .hotelCep(hotel.getAddress().getCep())
-      .hotelStreet(hotel.getAddress().getStreet())
-      .name(hotel.getName())
-      .description(hotel.getDescription())
-      .category(
-        HotelCategoryEntity.builder()
-          .id(hotel.getCategoryId().getValue())
-          .build()
-      )
-      .locality(
-        LocalityEntity.builder()
-          .id(hotel.getLocalityId().getValue())
-          .build()
-      )
-      .build();
-  }
+  Hotel hotelEntityToHotel(HotelEntity hotelEntity);
 
-  private Room roomEntityToRoom(final RoomEntity room) {
-    return Room.builder()
-      .id(RoomId.of(room.getId()))
-      .hotelId(HotelId.of(room.getHotel().getId()))
-      .name(room.getName())
-      .description(room.getDescription())
-      .currentPrice(Money.of(room.getCurrentPrice()))
-      .capacity(room.getCapacity())
-      .build();
-  }
+  HotelEntity hotelToHotelEntity(Hotel hotel);
 
-  public RoomEntity roomToRoomEntity(final Room room) {
-    return RoomEntity.builder()
-      .id(room.getId().getValue())
-      .currentPrice(room.getCurrentPrice().getValue())
-      .name(room.getName())
-      .description(room.getDescription())
-      .capacity(room.getCapacity())
-      .hotel(
-        HotelEntity.builder()
-          .id(room.getHotelId().getValue())
-          .build()
-      )
-      .build();
-  }
+  Room roomEntityToRoom(RoomEntity room);
+
+  RoomEntity roomToRoomEntity(Room room);
+
+  Set<RoomEntity> roomsToRoomEntitySet(Collection<? extends Room> rooms);
 
 }
---
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/data/db/mapper/HotelDatabaseMapperImpl.java
@@ -0,0 +1,92 @@
+package com.hotel.booking.system.hotel.service.data.db.mapper;
+
+import com.hotel.booking.system.hotel.service.core.domain.entity.Hotel;
+import com.hotel.booking.system.hotel.service.core.domain.entity.Room;
+import com.hotel.booking.system.hotel.service.core.domain.entity.Rooms;
+import com.hotel.booking.system.hotel.service.core.domain.valueobject.*;
+import com.hotel.booking.system.hotel.service.data.db.entity.HotelCategoryEntity;
+import com.hotel.booking.system.hotel.service.data.db.entity.HotelEntity;
+import com.hotel.booking.system.hotel.service.data.db.entity.LocalityEntity;
+import com.hotel.booking.system.hotel.service.data.db.entity.RoomEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class HotelDatabaseMapperImpl implements HotelDatabaseMapper {
+
+  @Override
+  public Hotel hotelEntityToHotel(final HotelEntity hotelEntity) {
+    return Hotel.builder()
+      .id(HotelId.of(hotelEntity.getId()))
+      .name(hotelEntity.getName())
+      .description(hotelEntity.getDescription())
+      .categoryId(HotelCategoryId.of(hotelEntity.getCategory().getId()))
+      .localityId(LocalityId.of(hotelEntity.getId()))
+      .address(new HotelAddress(hotelEntity.getHotelCep(), hotelEntity.getHotelStreet()))
+      .rooms(Rooms.newInstance(
+        hotelEntity.getRooms().stream()
+          .map(this::roomEntityToRoom)
+          .toList()
+      ))
+      .build();
+  }
+
+  @Override
+  public HotelEntity hotelToHotelEntity(final Hotel hotel) {
+    return HotelEntity.builder()
+      .id(hotel.getId().getValue())
+      .hotelCep(hotel.getAddress().getCep())
+      .hotelStreet(hotel.getAddress().getStreet())
+      .name(hotel.getName())
+      .description(hotel.getDescription())
+      .category(
+        HotelCategoryEntity.builder()
+          .id(hotel.getCategoryId().getValue())
+          .build()
+      )
+      .locality(
+        LocalityEntity.builder()
+          .id(hotel.getLocalityId().getValue())
+          .build()
+      )
+      .build();
+  }
+
+  @Override
+  public Room roomEntityToRoom(final RoomEntity room) {
+    return Room.builder()
+      .id(RoomId.of(room.getId()))
+      .hotelId(HotelId.of(room.getHotel().getId()))
+      .name(room.getName())
+      .description(room.getDescription())
+      .currentPrice(Money.of(room.getCurrentPrice()))
+      .capacity(room.getCapacity())
+      .build();
+  }
+
+  @Override
+  public RoomEntity roomToRoomEntity(final Room room) {
+    return RoomEntity.builder()
+      .id(room.getId().getValue())
+      .currentPrice(room.getCurrentPrice().getValue())
+      .name(room.getName())
+      .description(room.getDescription())
+      .capacity(room.getCapacity())
+      .hotel(
+        HotelEntity.builder()
+          .id(room.getHotelId().getValue())
+          .build()
+      )
+      .build();
+  }
+
+  @Override
+  public Set<RoomEntity> roomsToRoomEntitySet(final Collection<? extends Room> rooms) {
+    return rooms.stream()
+      .map(this::roomToRoomEntity)
+      .collect(Collectors.toSet());
+  }
+}
