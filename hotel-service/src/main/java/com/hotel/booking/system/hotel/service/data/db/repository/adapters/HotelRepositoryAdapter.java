1b40d5d3addcbe83f990dcb6b5e032b62d63079a
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-06-28 23:24:43 -0400

    fix: adjust implementation of `HotelRepositoryAdapter`

--- hotel-service/src/main/java/com/hotel/booking/system/hotel/service/data/db/repository/adapters/HotelRepositoryAdapter.java
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/data/db/repository/adapters/HotelRepositoryAdapter.java
@@ -4,6 +4,7 @@ import com.hotel.booking.system.hotel.service.core.domain.entity.Hotel;
 import com.hotel.booking.system.hotel.service.core.domain.valueobject.HotelCategoryId;
 import com.hotel.booking.system.hotel.service.core.ports.spi.HotelRepository;
 import com.hotel.booking.system.hotel.service.data.db.mapper.HotelDatabaseMapper;
+import com.hotel.booking.system.hotel.service.data.db.repository.HotelCategoryJpaRepository;
 import com.hotel.booking.system.hotel.service.data.db.repository.HotelJpaRepository;
 import com.hotel.booking.system.hotel.service.data.db.repository.RoomJpaRepository;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 public class HotelRepositoryAdapter implements HotelRepository {
 
   private final HotelJpaRepository hotelJpaRepository;
+  private final HotelCategoryJpaRepository hotelCategoryJpaRepository;
   private final RoomJpaRepository roomJpaRepository;
   private final HotelDatabaseMapper hotelDatabaseMapper;
 
@@ -21,11 +23,12 @@ public class HotelRepositoryAdapter implements HotelRepository {
   public void register(final Hotel newHotel) {
     final var newHotelEntity = this.hotelDatabaseMapper.hotelToHotelEntity(newHotel);
     this.hotelJpaRepository.save(newHotelEntity);
-    this.roomJpaRepository.saveAll(newHotelEntity.getRooms());
+    final var newRooms = this.hotelDatabaseMapper.roomsToRoomEntitySet(newHotel.getRoom());
+    this.roomJpaRepository.saveAll(newRooms);
   }
 
   @Override
   public boolean existsCategoryById(final HotelCategoryId hotelCategoryId) {
-    return this.hotelJpaRepository.existsByCategoryId(hotelCategoryId.getValue());
+    return this.hotelCategoryJpaRepository.existsById(hotelCategoryId.getValue());
   }
 }
