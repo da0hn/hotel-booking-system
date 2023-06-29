89c8a7cad72aea7e67832923af469a841bdc0444
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-06-28 20:32:28 -0400

    fix: add `roomId` and `hotelId` on `Room` to adjust `Hotel` validation

--- hotel-service/src/test/java/com/hotel/booking/system/hotel/service/core/domain/entity/HotelValidationTest.java
+++ hotel-service/src/test/java/com/hotel/booking/system/hotel/service/core/domain/entity/HotelValidationTest.java
@@ -2,21 +2,19 @@ package com.hotel.booking.system.hotel.service.core.domain.entity;
 
 import com.hotel.booking.system.commons.core.message.ApplicationMessage;
 import com.hotel.booking.system.hotel.service.core.domain.exception.HotelDomainException;
-import com.hotel.booking.system.hotel.service.core.domain.valueobject.HotelAddress;
-import com.hotel.booking.system.hotel.service.core.domain.valueobject.HotelCategoryId;
-import com.hotel.booking.system.hotel.service.core.domain.valueobject.LocalityId;
-import com.hotel.booking.system.hotel.service.core.domain.valueobject.Money;
+import com.hotel.booking.system.hotel.service.core.domain.valueobject.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @Tags({@Tag("unit")})
-@DisplayName("Hotel entity test")
+@DisplayName("Hotel entity validation tests")
 @ExtendWith(MockitoExtension.class)
-class HotelTest {
+class HotelValidationTest {
 
   private static final String HOTEL_RAW_ID = "ebaf3dbb-9b32-4dba-8872-becccd579ff9";
+  private static final HotelId HOTEL_ID = HotelId.of(HOTEL_RAW_ID);
   private static final String HOTEL_CATEGORY_RAW_ID = "44ba9f79-a581-441a-bc55-28a3cda3a8da";
   private static final HotelCategoryId CATEGORY_ID = HotelCategoryId.of(HOTEL_CATEGORY_RAW_ID);
   private static final String LOCALITY_RAW_ID = "ae348be4-4ccb-41c0-b00a-40363bc09cbb";
@@ -36,7 +34,7 @@ class HotelTest {
   @DisplayName("Should validate Hotel attributes")
   void test1() {
     this.sut = new Hotel(
-      null,
+      HOTEL_ID,
       HOTEL_NAME,
       HOTEL_DESCRIPTION,
       CATEGORY_ID,
@@ -44,18 +42,24 @@ class HotelTest {
       LOCALITY_ID,
       Rooms.of(
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Duplo Superior")
           .capacity(2)
           .description("This double room has a tile/marble floor and air conditioning.")
           .currentPrice(Money.of(350))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Triplo Standard")
           .capacity(3)
           .description("This triple room has air conditioning, dining area and tile/marble floor.")
           .currentPrice(Money.of(450))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Família Superior")
           .capacity(4)
           .description("Este quarto família dispõe de frigobar, piso frio/de mármore e ar-condicionado.")
@@ -63,19 +67,15 @@ class HotelTest {
           .build()
       )
     );
-    this.sut.validate();
-
-    Assertions.assertThat(this.sut.getCategoryId()).isEqualTo(CATEGORY_ID);
-    Assertions.assertThat(this.sut.getLocalityId()).isEqualTo(LOCALITY_ID);
-    Assertions.assertThat(this.sut.getRoom()).hasSize(3);
-    Assertions.assertThat(this.sut.getAddress()).isNotNull();
+    Assertions.assertThatCode(() -> this.sut.validate())
+      .doesNotThrowAnyException();
   }
 
   @Test
   @DisplayName("Should validate Hotel name")
   void test2() {
     this.sut = new Hotel(
-      null,
+      HOTEL_ID,
       null,
       HOTEL_DESCRIPTION,
       CATEGORY_ID,
@@ -83,18 +83,24 @@ class HotelTest {
       LOCALITY_ID,
       Rooms.of(
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Duplo Superior")
           .capacity(2)
           .description("This double room has a tile/marble floor and air conditioning.")
           .currentPrice(Money.of(350))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Triplo Standard")
           .capacity(3)
           .description("This triple room has air conditioning, dining area and tile/marble floor.")
           .currentPrice(Money.of(450))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Família Superior")
           .capacity(4)
           .description("Este quarto família dispõe de frigobar, piso frio/de mármore e ar-condicionado.")
@@ -111,7 +117,7 @@ class HotelTest {
   @DisplayName("Should validate Hotel description")
   void test3() {
     this.sut = new Hotel(
-      null,
+      HOTEL_ID,
       HOTEL_NAME,
       null,
       CATEGORY_ID,
@@ -119,18 +125,24 @@ class HotelTest {
       LOCALITY_ID,
       Rooms.of(
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Duplo Superior")
           .capacity(2)
           .description("This double room has a tile/marble floor and air conditioning.")
           .currentPrice(Money.of(350))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Triplo Standard")
           .capacity(3)
           .description("This triple room has air conditioning, dining area and tile/marble floor.")
           .currentPrice(Money.of(450))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Família Superior")
           .capacity(4)
           .description("Este quarto família dispõe de frigobar, piso frio/de mármore e ar-condicionado.")
@@ -147,7 +159,7 @@ class HotelTest {
   @DisplayName("Should validate Hotel cep")
   void test4() {
     this.sut = new Hotel(
-      null,
+      HOTEL_ID,
       HOTEL_NAME,
       HOTEL_DESCRIPTION,
       CATEGORY_ID,
@@ -155,18 +167,24 @@ class HotelTest {
       LOCALITY_ID,
       Rooms.of(
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Duplo Superior")
           .capacity(2)
           .description("This double room has a tile/marble floor and air conditioning.")
           .currentPrice(Money.of(350))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Triplo Standard")
           .capacity(3)
           .description("This triple room has air conditioning, dining area and tile/marble floor.")
           .currentPrice(Money.of(450))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Família Superior")
           .capacity(4)
           .description("Este quarto família dispõe de frigobar, piso frio/de mármore e ar-condicionado.")
@@ -183,7 +201,7 @@ class HotelTest {
   @DisplayName("Should validate Hotel street")
   void test5() {
     this.sut = new Hotel(
-      null,
+      HOTEL_ID,
       HOTEL_NAME,
       HOTEL_DESCRIPTION,
       CATEGORY_ID,
@@ -191,18 +209,24 @@ class HotelTest {
       LOCALITY_ID,
       Rooms.of(
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Duplo Superior")
           .capacity(2)
           .description("This double room has a tile/marble floor and air conditioning.")
           .currentPrice(Money.of(350))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Triplo Standard")
           .capacity(3)
           .description("This triple room has air conditioning, dining area and tile/marble floor.")
           .currentPrice(Money.of(450))
           .build(),
         Room.builder()
+          .id(RoomId.newInstance())
+          .hotelId(HOTEL_ID)
           .name("Quarto Família Superior")
           .capacity(4)
           .description("Este quarto família dispõe de frigobar, piso frio/de mármore e ar-condicionado.")
@@ -219,7 +243,7 @@ class HotelTest {
   @DisplayName("Should validate Hotel Rooms")
   void test6() {
     this.sut = new Hotel(
-      null,
+      HOTEL_ID,
       HOTEL_NAME,
       HOTEL_DESCRIPTION,
       CATEGORY_ID,
