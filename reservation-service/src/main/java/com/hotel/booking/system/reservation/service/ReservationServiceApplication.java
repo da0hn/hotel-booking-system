398ae726930dadbac8a2b306126935c98f90b85b
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-06-29 21:03:02 -0400

    chore: add `reservation-service` module

---
+++ reservation-service/src/main/java/com/hotel/booking/system/reservation/service/ReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.system.reservation.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(ReservationServiceApplication.class, args);
+  }
+
+}
