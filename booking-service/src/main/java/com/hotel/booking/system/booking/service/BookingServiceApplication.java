dcfcfe8e97934012ae401abf6ed0644a7515d8f0
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-07-03 20:59:30 -0400

    refactor: rename `reservation-service` to `booking-service`

--- booking-service/src/main/java/com/hotel/booking/system/booking/service/BookingServiceApplication.java
+++ booking-service/src/main/java/com/hotel/booking/system/booking/service/BookingServiceApplication.java
@@ -1,13 +1,13 @@
-package com.hotel.booking.system.reservation.service;
+package com.hotel.booking.system.booking.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReservationServiceApplication {
+public class BookingServiceApplication {
 
   public static void main(final String[] args) {
-    SpringApplication.run(ReservationServiceApplication.class, args);
+    SpringApplication.run(BookingServiceApplication.class, args);
   }
 
 }
