46320c0ed96353e7c1c17ab9cf865b1b074de7d1
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-07-04 22:28:41 -0400

    feat: create static constructor for raw id value and specific `UUID` value

--- hotel-service/src/main/java/com/hotel/booking/system/hotel/service/core/domain/valueobject/RoomId.java
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/core/domain/valueobject/RoomId.java
@@ -16,9 +16,14 @@ public class RoomId extends AbstractDomainEntityId<UUID> {
     return new RoomId(UUID.randomUUID());
   }
 
-  public static RoomId of(UUID value) {
+  public static RoomId of(final UUID value) {
     Objects.requireNonNull(value, ApplicationMessage.HOTEL_ROOM_NOT_NULL);
     return new RoomId(value);
+  }
+
+  public static RoomId of(final String rawValue) {
+    Objects.requireNonNull(rawValue, ApplicationMessage.HOTEL_ROOM_NOT_NULL);
+    return new RoomId(UUID.fromString(rawValue));
   }
 
   @Override
