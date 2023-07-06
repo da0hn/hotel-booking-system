3124d1cfccc3155358075d099855229a1f6c1bfe
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-07-06 14:09:04 -0400

    feat: add value object `CustomerId`

---
+++ hotel-service/src/main/java/com/hotel/booking/system/hotel/service/core/domain/valueobject/CustomerId.java
@@ -0,0 +1,28 @@
+package com.hotel.booking.system.hotel.service.core.domain.valueobject;
+
+import com.hotel.booking.system.commons.core.domain.AbstractDomainEntityId;
+import com.hotel.booking.system.commons.core.message.ApplicationMessage;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CustomerId extends AbstractDomainEntityId<UUID> {
+  protected CustomerId(final UUID value) {
+    super(value);
+  }
+
+  public static CustomerId newInstance() {
+    return new CustomerId(UUID.randomUUID());
+  }
+
+  public static CustomerId of(final UUID value) {
+    Objects.requireNonNull(value, ApplicationMessage.HOTEL_ROOM_NOT_NULL);
+    return new CustomerId(value);
+  }
+
+  public static CustomerId of(final String rawValue) {
+    Objects.requireNonNull(rawValue, ApplicationMessage.HOTEL_ROOM_NOT_NULL);
+    return new CustomerId(UUID.fromString(rawValue));
+  }
+
+}
