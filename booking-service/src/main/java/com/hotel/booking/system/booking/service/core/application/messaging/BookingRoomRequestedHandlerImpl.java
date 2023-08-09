7b7acdfd614f63b77fcb038b3e2ae0489e9bf0d1
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-08-08 22:16:53 -0400

    wip: add partial handling implementation of `BookingRoomOutput`

--- booking-service/src/main/java/com/hotel/booking/system/booking/service/core/application/messaging/BookingRoomRequestedHandlerImpl.java
+++ booking-service/src/main/java/com/hotel/booking/system/booking/service/core/application/messaging/BookingRoomRequestedHandlerImpl.java
@@ -1,9 +1,11 @@
 package com.hotel.booking.system.booking.service.core.application.messaging;
 
+import com.hotel.booking.system.booking.service.core.application.dto.BookingRoomOutput;
 import com.hotel.booking.system.booking.service.core.ports.api.mapper.BookingUseCaseMapper;
 import com.hotel.booking.system.booking.service.core.ports.api.messaging.BookingRoomRequestedHandler;
 import com.hotel.booking.system.booking.service.core.ports.api.usecase.BookingRoomUseCase;
 import com.hotel.booking.system.commons.core.domain.event.BookingRoomRequestedEvent;
+import com.hotel.booking.system.commons.core.domain.event.BookingRoomResponseEvent;
 
 public class BookingRoomRequestedHandlerImpl implements BookingRoomRequestedHandler {
 
@@ -20,9 +22,19 @@ public class BookingRoomRequestedHandlerImpl implements BookingRoomRequestedHand
 
   @Override
   public void handle(final BookingRoomRequestedEvent event) {
-    final var input = this.mapper.bookingRoomRequestedEventToBookingRoomInput(
-      event
-    );
-    this.bookingRoomUseCase.execute(input);
+    final var input = this.mapper.bookingRoomRequestedEventToBookingRoomInput(event);
+
+    final var output = this.bookingRoomUseCase.execute(input);
+
+    final var bookingRoomResponseEvent = this.mapOutputToEvent(output);
+    // TODO: enviar evento para o microsserviço `hotel-service`
+  }
+
+  private BookingRoomResponseEvent mapOutputToEvent(final BookingRoomOutput output) {
+    if (output.failureMessages().isNotEmpty()) {
+      return this.mapper.bookingRoomOutputToBookingRoomFailedEvent(output);
+    } else {
+      return this.mapper.bookingRoomOutputToBookingRoomResponseEvent(output);
+    }
   }
 }
