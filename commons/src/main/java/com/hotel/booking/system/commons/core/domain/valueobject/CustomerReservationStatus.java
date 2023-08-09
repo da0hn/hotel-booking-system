120bc16ae2ba7dcf24b2d4bf8cc86b79ea2ad82d
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-08-08 22:11:31 -0400

    feat: add `AWAITING_PAYMENT` status

--- commons/src/main/java/com/hotel/booking/system/commons/core/domain/valueobject/CustomerReservationStatus.java
+++ commons/src/main/java/com/hotel/booking/system/commons/core/domain/valueobject/CustomerReservationStatus.java
@@ -3,6 +3,7 @@ package com.hotel.booking.system.commons.core.domain.valueobject;
 public enum CustomerReservationStatus {
 
   AWAITING_RESERVATION,
+  AWAITING_PAYMENT,
   RESERVED,
   RESERVATION_FAILED
 }
