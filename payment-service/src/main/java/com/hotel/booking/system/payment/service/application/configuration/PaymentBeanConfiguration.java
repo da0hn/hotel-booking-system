8ea961d87054ae36bc9ca3a538fd6d72e80be1d4
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-08-19 12:01:11 -0400

    refactor: rename `PaymentApplicationConfiguration` to `PaymentBeanConfiguration`

--- payment-service/src/main/java/com/hotel/booking/system/payment/service/application/configuration/PaymentBeanConfiguration.java
+++ payment-service/src/main/java/com/hotel/booking/system/payment/service/application/configuration/PaymentBeanConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PaymentApplicationConfiguration {
+public class PaymentBeanConfiguration {
 
   @Bean
   public PaymentUseCaseMapper paymentUseCaseMapper() {
